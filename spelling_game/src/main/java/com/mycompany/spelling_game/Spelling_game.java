c7be1ea73e49f1049d81cd207182544b5b154071
AnnaN <anguy334@uwo.ca>
2026-03-15 16:09:24 -0400

    testing

---
+++ spelling_game/src/main/java/com/mycompany/spelling_game/Spelling_game.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.spelling_game;
+
+/**
+ *
+ * @author annanguyen
+ */
+public class Spelling_game {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
